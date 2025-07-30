8d96a9fa9c0fc148f4d0f0cb1107bfc3148fdfc5
lavnay852 <cblavanya94@gmail.com>
2025-07-30 09:42:50 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,14 @@
+a=int(input("enter the first number",))
+b=int(input("enter the second number",))
+c=int(input("enter the third number",))
+d=int(input("enter the fourth number",))
+eq1=((a+b+c)*c)/d
+eq2=(a*b)+(a/b)-(a%b)
+eq3=(a+b)-(c*d)-(a/b)+(c-d)
+eq4=((a*b*c)/d)-((a/b)*c)+d
+print("equation number is",eq1)
+print("equation number is",eq2)
+print("eqation number is",eq3)
+print("equation number is",eq4)
+
+               
